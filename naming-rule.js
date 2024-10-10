9aa584027fab63da53534510469b9872363fafbc
AdithyaS-git <adithyasuresh635@gmail.com>
2024-10-10 13:45:18 +0530

    Add files via upload

---
+++ naming-rule.js
@@ -0,0 +1,13 @@
+//Valid variable names
+let firstName="John"
+let userAge=30;
+let isStudent=true
+let _privateVar="secret";
+let $specialVar="important";
+
+//Invalid variable names
+
+// let 123var ="Invalid"; //error variable names cannot start with num
+// let my-var ="Invalid";//Error:variable name cannot contain-
+// let my variable="Invalid";//Error:space
+// let for ="invalid";Error:reserved keyword
